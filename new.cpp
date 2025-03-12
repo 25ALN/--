2c18ceb70e3240cf0d4215c3bd83ae7ae79fb3df
25ALN <3076331747@qq.com>
2025-03-12 22:08:59 +0800

    learn

---
+++ new.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// int main(){
+//     using namespace std;
+//     int age;
+//     cout << "please enter youe age:";
+//     cin >> age;
+//     cout << "you are " << age << " old!"<<endl;
+// return 0;
+// }
+
+int main(){
+    float n=5.0;
+    float x=3.1;
+    cout << n/x <<endl;
+    return 0;
+}
