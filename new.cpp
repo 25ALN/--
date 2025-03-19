34492d13b20dd7ebb6311b071a1ead5212fc1ae7
25ALN <3076331747@qq.com>
2025-03-19 21:48:09 +0800

    cpp

--- new.cpp
+++ new.cpp
@@ -11,10 +11,39 @@ using namespace std;
 // return 0;
 // }
 
+// int main(){
+//     string str1="hello";
+//     string str2="world";
+//     cout << str1+str2<<endl;
+//     cout << str1.size();
+//     std :: cout<< "head";
+//     return 0;
+// }
+
+// int main(){
+//     unsigned u=10;
+//     int i=30,i2=20;
+    
+//     std :: cout << i-u << std :: endl;
+//     std :: cout << u-i << std :: endl;
+//     std :: cout << i-i2 << std :: endl;
+//     std :: cout << i2-i << std :: endl;
+//     return 0;
+// }
+
+//作用域的测试
+// int i=42;
+// int main(){
+//     std::cout << i<<endl;
+//     int i=0;
+//     std :: cout << i<<endl;
+//     std :: cout <<::i<<endl;
+//     return 0;
+// }
+
 int main(){
-    string str1="hello";
-    string str2="world";
-    cout << str1+str2<<endl;
-    cout << str1.size();
+    int i,&j=i;
+    i=5;j=10;
+    cout << i << " " << j;
     return 0;
 }
